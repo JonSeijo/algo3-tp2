e300242efc963dd037b73c3ec2831e58253d1c37
jonseijo <jon.seijo@gmail.com>
2017-05-17 12:46:22 -0300

    Continua experimentador

--- experimentador3.py
+++ experimentador3.py
@@ -16,22 +16,40 @@ import sys
 
 ejecutable = "./tiempos3"
 
+input_tmp = "./experimentos/input.tmp"
+
 csv_random = "./experimentos/problema3/random.csv"
-repes_random = 50
+repes_random = 25
 n_max_random = 100
 tipo_random = "RANDOM"
 
+# Genera un input del tipo en input_tmp de tamaño n
+def generate_input(tipo, n):
+    if tipo == tipo_random:
+        # aca guardo un input random en input_tmp de tamaño n
+        print("Deberia generar input tam " + str(n))
+
+    else:
+        sys.exit("Tipo invalido en generacion de input")
+
+
 # Experimentacion generica, que varia dependiendo de los parametros que se le pasen
-def experimentar(csv, repes, n_max, tipo):
+def experimentar(csv_filename, repes, n_max, tipo):
     # Guardar encabezado de csv
-    # Para cada repes_random
-        # Generar el input falopa en un archivo
-        # Correr ejecutable con el input falopa
-        # Guardar en un csv por linea
-        #     n; tiempo;
+    with open(csv_filename, 'w') as csv :
+        csv.write("n;tiempo;")
+
+    # Para cada tamaño
+    for n in range(2, n_max):
+        # Para cada repes_random de cada tamaño
+        for repe in range(repes):
+            # Generar el input falopa en un input_tmp
+            generate_input(tipo, n)
+            # Correr ejecutable con el input falopa
+            # Guardar en un csv por linea
+            #     n; tiempo;
 
     print ("Experimento random")
-
 
 
 if __name__ == '__main__':
@@ -42,7 +60,5 @@ if __name__ == '__main__':
     if not len(sys.argv) > 1:
         sys.exit("No arguments passed! Use -h flag for help")
 
-
     if args.random:
         experimentar(csv_random, repes_random, n_max_random, tipo_random)
-
