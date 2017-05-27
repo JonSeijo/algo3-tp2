0815aa2994921ab54dbf12b8ed677544b3e26f9c
jonseijo <jon.seijo@gmail.com>
2017-05-27 12:23:09 -0300

    Exporta imagenes de problema 2 a .pdf

--- graficador2.py
+++ graficador2.py
@@ -16,15 +16,15 @@ tiempos_aristas_maximas = data_aristas_maximas.groupby('n')['tiempo'].mean() / 1
 # Aristas maximas, aristas random y aristas minimas en el mismo grafico
 # --------------------------------------------------------------------------------------------------------------------
 plt.clf()
-plot_aristas_maximas = tiempos_aristas_maximas.plot(fontsize = 13, color='#C44E52') # Color rojo
+plot_aristas_maximas = tiempos_aristas_maximas.plot(fontsize = 13, figsize=(11,8), color='#C44E52') # Color rojo
 plot_aristas_maximas.set_title('\n Problema 2 \n Tiempo medio para diferentes tamaños de grafos', fontsize = 15)
-plot_aristas_maximas.set_xlabel("Cantidad de elementos", size = 14)
+
 plot_aristas_maximas.set_ylabel("Segundos", size = 14)
 
 tiempos_random_todo.plot(ax=plot_aristas_maximas, color='#4C72B0') # Color azul
 tiempos_aristas_minimas.plot(ax=plot_aristas_maximas, color='#55A868') # Color verde
 
-plot_aristas_maximas.legend(['Aristas maximas', 'Aristas random', 'Aristas minimas'], fontsize = 14)
-
+plot_aristas_maximas.legend(['Aristas: maximas', 'Aristas: random', 'Aristas: minimas'], fontsize = 14)
+plot_aristas_maximas.set_xlabel("Cantidad de elementos", size = 14)
 plt.show()
 # --------------------------------------------------------------------------------------------------------------------
