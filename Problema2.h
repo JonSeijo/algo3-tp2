c2bb1a9a062f261dee62dfd9560ef0590e7310ce
jonseijo <jon.seijo@gmail.com>
2017-05-28 12:24:17 -0300

    Muevo idea de resolucion al .cpp, pues esta mas a la vista

--- Problema2.h
+++ Problema2.h
@@ -1,34 +1,7 @@
 #ifndef H_PROBLEMA_2
 #define H_PROBLEMA_2
 
-// El main no está en el problema2,
-// sino que habrá un main en solucion.cpp (que llama al problema2)
-// y otro main en tiempos.cpp (que llama al problema2)
-
-/* Idea
-    Sé que hay al menos algún ciclo. Si tomo el costo máximo y disminuyo los
-    peajes en dicha cantidad más uno, entonces todos los pesos quedan
-    negativos, por lo que el ciclo que existía queda negativo, y hay un
-    abuso. Si disminuyo los peajes en 0, como parto de un grafo sin abuso, no
-    tengo abuso. Intuitivamente, el valor máximo que busco está en el medio.
-    Como los números del 0 al c están ordenados, puedo hacer búsqueda binaria.
-
-    Hay un detalle, y es que el grafo no necesariamente es fuertemente
-    conexo, con lo no podemos elegir cualquier nodo inicial. Para resolver
-    este problema, creamos un nuevo nodo y lo contectamos con un eje de ida
-    a todos los nodos del grafo. Es en este nuevo grafo es en donde buscamos
-    un abuso. 
-
-Algoritmo
-    - Parto del rango [1..c+1) y aplico búsqueda binaria. En cada rango no
-    trivial, llamo a la función 'hayAbuso' con el elemento en el medio. Si
-    da verdadero, busco en la parte superior del rango (con c inclusive), 
-    y si da falso, en la parte interior (con c no inclusive).
-    - En 'hayAbuso', aplico Ford desde un nodo cualquiera, pero considerando
-    el costo de cada eje como el original menos c. Hago n+1 iteraciones
-    completas. Si hay un cambio de las distancias en la última iteración,
-    tengo un ciclo negativo y hay abuso, de lo contrario no lo hay.
-*/
+// El main esta en ej2.cpp
 
 #include <iostream>
 #include <vector>
